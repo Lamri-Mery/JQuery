a75c94e0536de0fafd50d4038a25909010f16135
Lamri Mery <Lamri.Mery@gmail.com>
2018-05-22 21:40:18 +0200

    Test section menu

---
+++ partials/js/menujquery.js
@@ -0,0 +1,8 @@
+$(function(){
+//Quand le document est prêt
+	$('.main-header').click(function(){
+	//Séléctionne l'élément de classe main-header et quand tu cliques dessus
+		$('.main-header').hide();
+		//Séléctionne l'élément de classe main-header et cache là
+	});
+});
