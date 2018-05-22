7140a2d3ac3197236df72d5fd78d260756bac45c
Lamri Mery <Lamri.Mery@gmail.com>
2018-05-23 00:02:44 +0200

    Test Réussi Scroll vericale

--- partials/js/menujquery.js
+++ partials/js/menujquery.js
@@ -7,8 +7,10 @@ $(function(){
 		// Sélectionne la page, quand tu scroll
 		/* Act on the event */
 		var montop = $(this).scrollTop();
-		//montop est égale à cette élément tu obtenir la position verticale actuelle de la barre de défilement quand tu scroll
-
+		//montop est égale à cette élément, quand tu obtiens la position verticale actuelle de la barre de défilement quand tu scroll
+			
+			console.log(montop);
+			
 			if(myScrollTop < montop){
 			// Si myScrollTop est plus petit que montop
 				$('p').css('color','red');
@@ -18,5 +20,7 @@ $(function(){
 				$('p').css('color','black');
 				// Sélectionne les paragraphes, prend leur css et change la couleur en sinon
 			}
+			myScrollTop = montop
+			// J'indique myScrollTop est égale à la position verticale actuelle	
 	});
 });
