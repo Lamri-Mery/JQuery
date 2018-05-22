c5404d11969c63b99136bff64b6b51c483d48f62
Lamri Mery <Lamri.Mery@gmail.com>
2018-05-22 22:20:34 +0200

    Test modification CSS quand tu Scroll

--- partials/js/menujquery.js
+++ partials/js/menujquery.js
@@ -1,8 +1,22 @@
 $(function(){
-//Quand le document est prêt
-	$('.main-header').click(function(){
-	//Séléctionne l'élément de classe main-header et quand tu cliques dessus
-		$('.main-header').hide();
-		//Séléctionne l'élément de classe main-header et cache là
+// Quand le document est prêt
+	var myScrollTop = 0;
+	// myScrollTop est égale à 0
+
+	$(window).scroll(function() {
+		// Sélectionne la page, quand tu scroll
+		/* Act on the event */
+		var montop = $(this).scrollTop();
+		//montop est égale à cette élément quand tu scroll de haut en bas
+		
+			if(myScrollTop < montop){
+			// Si montop est plus petit que montop
+				$('p').css('color','red');
+				// Sélectionne les paragraphes, prend leur css et change la couleur en rouge
+			}else{
+			// Ou autre
+				$('p').css('color','black');
+				// Sélectionne les paragraphes, prend leur css et change la couleur en sinon
+			}
 	});
 });
